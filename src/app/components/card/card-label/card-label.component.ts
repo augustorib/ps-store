7ba540cfebecc6d1df00797bf9afb606818943f0
augustorib <augustorifra@gmail.com>
2024-10-10 04:36:26 -0300

    feat: criando e customizando o card-label

--- src/app/components/card/card-label/card-label.component.ts
+++ src/app/components/card/card-label/card-label.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-label',
@@ -9,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class CardLabelComponent {
 
+  @Input()
+  gameLabel: string = ""
+  
 }
