b22dd048df8f4c12d39b7ea0ba02b5de05dc1d71
augustorib <augustorifra@gmail.com>
2024-10-16 00:14:49 -0300

    feat: configurando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { WhishlistComponent } from './pages/whishlist/whishlist.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'whishlist', component: WhishlistComponent, pathMatch: 'full'}
+];
+        
